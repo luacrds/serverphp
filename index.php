5104d74f770de4e7e92f062238cf800a10ef45a1
Luana Cardoso <104872684+luacrds@users.noreply.github.com>
2023-04-27 22:29:14 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset= "UTF-8">
+    <title>Login</title>
+</head>
+<body>
+
+<h1> Login </h1>
+    <form method= "post" action= "main.php">
+        <label for= "name"> Nome Usuario:</label>
+        <input type="text" name="nome" required> <br>
+
+        <label for= "pass">Senha:</label>
+        <input type="password" name="pass"></textarea><br>
+
+        <input type="submit" value="Enviar" name="btnSubmitInsertTaskForm">
+</form>
+</body>
+</html>
